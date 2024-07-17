2b894ac1c4a55b5e4b8ebd4b73e0387c91e06da3
MeansKane <phamminhkhangpy2003@gmail.com>
2024-07-17 17:28:16 +0700

    demo delegation 1

--- src/main/kotlin/delegation/demo_delegation.kt
+++ src/main/kotlin/delegation/demo_delegation.kt
@@ -1,5 +1,28 @@
 package delegation
 
+interface Base {
+    fun printVal()
+}
+
+class BaseImpl (private val x: Int) : Base {
+    override fun printVal() {
+        println("Value: $x")
+    }
+}
+
+class OtherBaseImpl (private val x: Int) :Base {
+    override fun printVal() {
+        println("Other value: $x")
+    }
+
+}
+
+class Derived (b: Base) : Base by b
+
 fun main() {
-    println("a")
+    val b = BaseImpl(19)
+    Derived(b).printVal()
+
+    val b1 = OtherBaseImpl(100)
+    Derived(b1).printVal()
 }
