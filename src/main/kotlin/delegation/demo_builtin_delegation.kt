975494a9709516e27ef411f0c013ca09efc75b42
MeansKane <phamminhkhangpy2003@gmail.com>
2024-07-17 23:39:26 +0700

    demo builtin delegation

---
+++ src/main/kotlin/delegation/demo_builtin_delegation.kt
@@ -0,0 +1,18 @@
+package delegation
+
+import kotlin.properties.Delegates
+
+class User {
+    var name: String by Delegates.observable("<no name>") { _, old, new ->
+        println("$old -> $new")
+    }
+    override fun toString(): String {
+        return "User(name='$name')"
+    }
+}
+
+fun main() {
+    val user = User()
+    println(user)
+}
+
