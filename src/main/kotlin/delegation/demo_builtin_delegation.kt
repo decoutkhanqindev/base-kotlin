9d4d462200391a6fec8ba3f1ca528d3fb56214e3
MeansKane <phamminhkhangpy2003@gmail.com>
2024-07-17 23:54:35 +0700

    Delegates.vetoable()

--- src/main/kotlin/delegation/demo_builtin_delegation.kt
+++ src/main/kotlin/delegation/demo_builtin_delegation.kt
@@ -12,23 +12,33 @@ class User {
 
     // lang nghe su thay doi, return boolean
     var age: Int by Delegates.vetoable(0) { property, oldValue, newValue ->
+        println("onChanged: $oldValue -> $newValue")
+        // validation
         newValue >= 0
     }
 
     override fun toString(): String {
-        return "User(name='$name', '$address')"
+        return "User(name='$name', address='$address', age='$age')"
     }
 }
 
 
 fun main() {
     val user = User()
-//    user.name = "abc"
-//    println(user)
-//    user.name = "xyz"
-//    println(user)
+    user.address = "<no-name>"
+    user.name = "abc"
+    println(user)
+    user.name = "xyz"
+    println(user)
 
     user.address = "vn"
+    println(user)
+    user.address = "vn2"
+    println(user)
+
+    user.age = -1
+    println(user)
+    user.age = 19
     println(user)
 }
 
