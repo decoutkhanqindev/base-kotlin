76599553591abf5237fccc8dbbd8d5bc540999dc
MeansKane <phamminhkhangpy2003@gmail.com>
2024-07-17 23:11:34 +0700

    SYNCHRONIZED default mode

--- src/main/kotlin/delegation/demo_lazy_delegate_thred_safe_mode.kt
+++ src/main/kotlin/delegation/demo_lazy_delegate_thred_safe_mode.kt
@@ -1,22 +1,30 @@
 package delegation
 
+import kotlin.concurrent.thread
+
 class LazySample1 {
     init {
         println("created!")
     }
 
-    private val lazy: Lazy<String> = lazy {
+    // LazyThreadSafetyMode.SYNCHRONIZED default mode
+    private val lazy: Lazy<String> = lazy(LazyThreadSafetyMode.SYNCHRONIZED) {
         println("computed!")
         "my lazy"
     }
 
-    val lazyStr: String by lazy
+    val lazyStr: String by lazy // lazy.value
 }
 
 fun main() {
     val sample = LazySample()
-    println(sample.lazyStr)
-    println(sample.lazyStr)
-    println(sample.lazyStr)
-    println(sample.lazyStr)
+    val threads = List(1000) {
+        thread {
+            repeat(100) {
+                println(sample.lazyStr)
+            }
+        }
+    }
+    threads.forEach() { it.join() }
+    println("Thread is done!")
 }
