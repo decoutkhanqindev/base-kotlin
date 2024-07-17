d51b9aa3af7f9b2642f4b88f4cf6bf938b569961
MeansKane <phamminhkhangpy2003@gmail.com>
2024-07-17 23:26:16 +0700

    PUBLICATION mode

--- src/main/kotlin/delegation/demo_lazy_delegate_thred_safe_mode.kt
+++ src/main/kotlin/delegation/demo_lazy_delegate_thred_safe_mode.kt
@@ -1,16 +1,16 @@
 package delegation
 
 import kotlin.concurrent.thread
-
+import kotlin.random.Random
 class LazySample1 {
     init {
         println("created!")
     }
 
-//    LazyThreadSafetyMode.PUBLICATION
+    //    LazyThreadSafetyMode.PUBLICATION
     private val lazy: Lazy<String> = lazy(LazyThreadSafetyMode.PUBLICATION) {
         println("computed!")
-        "my lazy"
+        "my lazy" + Random.nextInt()
     }
 
     val lazyStr: String by lazy // lazy.value
@@ -18,13 +18,13 @@ class LazySample1 {
 
 fun main() {
     val sample = LazySample1()
-    val threads = List(1000) {
+    val threads = List(100) {
         thread {
             repeat(100) {
                 println(sample.lazyStr)
             }
         }
     }
-    threads.forEach() { it.join() }
-    println("Thread is done!")
+    threads.forEach { it.join() }
+    println("OK")
 }
