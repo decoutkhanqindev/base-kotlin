44e8a774f2b271aca053fc3552edd558c48e3159
MeansKane <phamminhkhangpy2003@gmail.com>
2024-07-17 23:47:08 +0700

    Delegates.notNull()

--- src/main/kotlin/delegation/demo_builtin_delegation.kt
+++ src/main/kotlin/delegation/demo_builtin_delegation.kt
@@ -6,16 +6,21 @@ class User {
     var name: String by Delegates.observable("<no name>") { property, old, new ->
         println("onChanged: $old -> $new")
     }
+
+    var address:String by Delegates.notNull() // lateinit val
     override fun toString(): String {
-        return "User(name='$name')"
+        return "User(name='$name', '$address')"
     }
 }
 
 fun main() {
     val user = User()
-    user.name = "abc"
-    println(user)
-    user.name = "xyz"
+//    user.name = "abc"
+//    println(user)
+//    user.name = "xyz"
+//    println(user)
+
+    user.address = "vn"
     println(user)
 }
 
