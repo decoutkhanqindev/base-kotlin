afd9820e6d7c8c2ccac62e2f9eb1fe8bbe895664
MeansKane <phamminhkhangpy2003@gmail.com>
2024-07-17 23:51:01 +0700

    Delegates.vetoable()

--- src/main/kotlin/delegation/demo_builtin_delegation.kt
+++ src/main/kotlin/delegation/demo_builtin_delegation.kt
@@ -3,15 +3,21 @@ package delegation
 import kotlin.properties.Delegates
 
 class User {
-    var name: String by Delegates.observable("<no name>") { property, old, new ->
-        println("onChanged: $old -> $new")
+    var name: String by Delegates.observable("<no name>") { property, oldValue, newValue ->
+        println("onChanged: $oldValue -> $newValue")
     }
 
-    var address:String by Delegates.notNull() // lateinit val
+    var address: String by Delegates.notNull() // ~~ lateinit val
+
+    var age: Int by Delegates.vetoable(0) { property, oldValue, newValue ->
+        newValue >= 0
+    }
+
     override fun toString(): String {
         return "User(name='$name', '$address')"
     }
 }
+
 
 fun main() {
     val user = User()
