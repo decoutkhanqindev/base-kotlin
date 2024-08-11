056f36222595ed3d0ec2bc36f4dcbd93427143c2
MeansKane <phamminhkhangpy2003@gmail.com>
2024-08-12 01:50:09 +0700

    coroutine -> job

--- src/main/kotlin/threadAndCoroutine/ex9.kt
+++ src/main/kotlin/threadAndCoroutine/ex9.kt
@@ -1,5 +1,14 @@
 package threadAndCoroutine
 
-fun main() {
+import kotlinx.coroutines.CancellationException
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.job
+
+fun main() = runBlocking {
 
 }
