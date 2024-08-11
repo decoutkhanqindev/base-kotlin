6140f102d7a610e12df034ebb3cffc82369d44f3
MeansKane <phamminhkhangpy2003@gmail.com>
2024-08-11 23:51:16 +0700

    coroutine -> coroutine context

--- src/main/kotlin/threadAndCoroutine/ex5.kt
+++ src/main/kotlin/threadAndCoroutine/ex5.kt
@@ -1,11 +1,26 @@
 ﻿package threadAndCoroutine
 
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.currentCoroutineContext
 import kotlinx.coroutines.runBlocking
+import kotlin.coroutines.CoroutineContext
 
+@OptIn(ExperimentalStdlibApi::class)
 suspend fun demoCoroutineContext() {
-    val ctx = currentCoroutineContext()
+    val ctx: CoroutineContext = currentCoroutineContext()
     println(ctx)
+
+    val dispatcher: CoroutineDispatcher? = ctx[CoroutineDispatcher.Key]
+    print(dispatcher)
+
+    val count: Int = ctx.fold(0) { acc, e ->
+        println("acc=$acc, e=$e")
+        acc + 1
+    }
+    println("count=$count")
+
+    val minusDispatcher: CoroutineContext? = ctx.minusKey(CoroutineDispatcher.Key)
+    println("minusDispatcher=$minusDispatcher")
 }
 
 fun main() = runBlocking {
