46badfed615b5cd442ba56d06cb477cf360f6e33
MeansKane <phamminhkhangpy2003@gmail.com>
2024-08-12 02:03:13 +0700

    coroutine -> job

--- src/main/kotlin/threadAndCoroutine/ex9.kt
+++ src/main/kotlin/threadAndCoroutine/ex9.kt
@@ -1,14 +1,37 @@
 package threadAndCoroutine
 
-import kotlinx.coroutines.CancellationException
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.cancel
+import kotlinx.coroutines.cancelAndJoin
 import kotlinx.coroutines.job
+import kotlinx.coroutines.currentCoroutineContext
 
-fun main() = runBlocking {
+@OptIn(ExperimentalCoroutinesApi::class)
+fun main(): Unit = runBlocking {
+    val coroutineScope: CoroutineScope = CoroutineScope(context = Dispatchers.IO)
+    val parentJob = coroutineScope.coroutineContext.job
+    println("parentJob=$parentJob")
 
+    val job: Job = coroutineScope.launch {
+        println("Start task ... ${currentCoroutineContext().job}")
+        delay(2000)
+        println("End task ... ${currentCoroutineContext().job}")
+    }
+
+    println("job=$job")
+    println("-".repeat(100))
+
+    println("isActive: ${job.isActive}, isCompleted=${job.isCompleted}, isCancelled=${job.isCancelled} --- parent=${job.parent}")
+    delay(100)
+    job.cancel()
+    job.join()
+    println("isActive: ${job.isActive}, isCompleted=${job.isCompleted}, isCancelled=${job.isCancelled} --- parent=${job.parent}")
+
+    delay(3000)
 }
