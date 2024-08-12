a729a7faee1eccdcf150a5cbe8e8d03f2a1b276e
MeansKane <phamminhkhangpy2003@gmail.com>
2024-08-12 14:54:12 +0700

    coroutine -> handle exception by async

--- src/main/kotlin/threadAndCoroutine/ex11.kt
+++ src/main/kotlin/threadAndCoroutine/ex11.kt
@@ -2,6 +2,7 @@ package com.rxmobileteam.course006.lecture06
 
 import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Deferred
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.async
@@ -11,24 +12,26 @@ import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 
 fun main(): Unit = runBlocking {
+    // handle exception in launch phai can handle dua tren ket qua tra ve = Deferred
+
     val scope: CoroutineScope = CoroutineScope(
         context = Dispatchers.Default + Job()
     )
 
-    val d1 = scope.async<Int>{
+    val d1: Deferred<Int> = scope.async<Int> {
         println("launch 1")
         delay(1)
         println("launch 1 throws...")
         throw RuntimeException("launch 1 failed")
     }
 
-    val d2 = scope.async {
+    val d2: Deferred<Unit> = scope.async {
         println("launch 2")
         delay(1000)
         println(">>> launch 2 done")
     }
 
-    val d3 = scope.async {
+    val d3: Deferred<Unit> = scope.async {
         println("launch 3")
         delay(100)
         println(">> launch 3 done")
@@ -36,14 +39,20 @@ fun main(): Unit = runBlocking {
 
     try {
         d1.await()
-    }catch (e: RuntimeException){
+    } catch (e: RuntimeException) {
         println("d1 failed: $e")
     }
 
     try {
         d2.await()
-    }catch (e: RuntimeException){
+    } catch (e: RuntimeException) {
         println("d2 failed: $e")
+    }
+
+    try {
+        d3.await()
+    } catch (e: Exception) {
+        println("d3 failed: $e")
     }
 
     delay(5_000)
