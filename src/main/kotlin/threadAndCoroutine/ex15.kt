bb6b5e5b0d861f36c4318ff67d0aef8ce565661e
MeansKane <phamminhkhangpy2003@gmail.com>
2024-08-12 14:16:06 +0700

    coroutine -> Run non-cancellable block

--- src/main/kotlin/threadAndCoroutine/ex15.kt
+++ src/main/kotlin/threadAndCoroutine/ex15.kt
@@ -22,10 +22,14 @@ fun main(): Unit = runBlocking {
                 delay(500L)
             }
         } finally {
-            withContext(NonCancellable) {
+            withContext(NonCancellable) { // chuyen sang dispatcher khac va tra ve 1 ket qua
                 println("job: I'm running finally")
                 delay(1000L)
                 println("job: And I've just delayed for 1 sec because I'm non-cancellable")
+                // neu khong boc ben trong cua withContext(NonCancellable)
+                // thi chi in ra job: I'm running finally
+                // vi delay(1000L) la suspend function no se check xem job isActive hay khong
+                // trong truong hop nay notActive nen se khong in ra job: And I've just delayed for 1 sec because I'm non-cancellable
             }
         }
     }
