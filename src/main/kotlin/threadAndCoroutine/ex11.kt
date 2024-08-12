ab0fc818ae494cc54c784986ca8c505b640de4dc
MeansKane <phamminhkhangpy2003@gmail.com>
2024-08-12 13:39:10 +0700

    coroutine -> start coroutine by async

--- src/main/kotlin/threadAndCoroutine/ex11.kt
+++ src/main/kotlin/threadAndCoroutine/ex11.kt
@@ -1,25 +1,50 @@
-package threadAndCoroutine
+package com.rxmobileteam.course006.lecture06
 
+import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Deferred
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.Job
-import kotlinx.coroutines.currentCoroutineContext
+import kotlinx.coroutines.async
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.job
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.async
 
 fun main(): Unit = runBlocking {
-    val scope: CoroutineScope = CoroutineScope(Dispatchers.IO + Job())
+    val scope: CoroutineScope = CoroutineScope(
+        context = Dispatchers.Default + Job()
+    )
 
-    val deferredString: Deferred<String> = scope.async {
-        delay(1000)
-        "hello"
+    val d1 = scope.async<Int>{
+        println("launch 1")
+        delay(1)
+        println("launch 1 throws...")
+        throw RuntimeException("launch 1 failed")
     }
 
-    val result = deferredString.await()
-    println(result)
+    val d2 = scope.async {
+        println("launch 2")
+        delay(1000)
+        println(">>> launch 2 done")
+    }
+
+    val d3 = scope.async {
+        println("launch 3")
+        delay(100)
+        println(">> launch 3 done")
+    }
+
+    try {
+        d1.await()
+    }catch (e: RuntimeException){
+        println("d1 failed: $e")
+    }
+
+    try {
+        d2.await()
+    }catch (e: RuntimeException){
+        println("d2 failed: $e")
+    }
+
+    delay(5_000)
 }
